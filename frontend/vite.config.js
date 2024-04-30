c90f98f7363368a49f98c7772db778ca139b7132
unnikrishnansk <unni.sk94@gmail.com>
2024-04-30 09:21:25 +0530

    port changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     port: 3000,   
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        // target: 'http://localhost:5000',
+        target: 'http://http://13.234.34.97:5000',
         changeOrigin: true
       }
     }
